a4e0884b0b582b33e8f7a7aff4c10bb70bbec5d5
41118110 <148747989+41118110@users.noreply.github.com>
2023-12-25 15:24:33 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+$(document).ready(function () {
+  $("#btn1").click(function () {
+    $("#student-id, #student-name").css({ "color": "blue", "font-size": "20px" });
+  });
+
+  $("#btn2").click(function () {
+    $("#profile-pic").css({ "border": "5px solid red", "border-radius": "20%" });
+  });
+
+  $("#btn3").click(function () {
+    $("#profile-pic").attr("src", "41118110-2.jpg");
+  });
+
+  $("#btn4").click(function () {
+    $("#profile-pic").fadeOut(1000, function () {
+      $(this).fadeIn(1000);
+    });
+  });
+});
